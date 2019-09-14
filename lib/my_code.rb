97c43f8e89ab80d677907818a48b7c849ca48d1a
Vannida Lim <vannida731@gmail.com>
2019-09-14 05:09:56 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -15,6 +15,9 @@ def reduce(source_array, starting_value = nil)
     accumulator = source_array[0]
   end
   source_array.length.times { |index|
+    if (index == 0) {
+      continue
+    }
     accumulator = yield(accumulator, source_array[index])
   }
    return accumulator
