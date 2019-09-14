5c17833285b176a91b2183f471e686bbcc42b02a
Vannida Lim <vannida731@gmail.com>
2019-09-14 04:51:00 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,22 @@
 # Your Code Here
+def map (array)
+  new_array = []
+  array.length.times { |index|
+    transformed_value = yield(array[index])
+    new_array << transformed_value
+  }
+  return new_array
+end
+
+def reduce(source_array, starting_value = nil)
+  if starting_value 
+    reduction = starting_value
+    i = 0
+  else reduction = source_array[0]
+    i = 1 
+  end
+  array.length.times { |index|
+    reduction = yield(reduction, source_array[i])
+    }
+   reduction
+end 
